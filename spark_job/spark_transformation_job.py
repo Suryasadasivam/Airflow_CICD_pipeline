2593565315fc61d6d83a45f4f9e6740cafcd9521
Surya Sadasivam <162082177+Suryasadasivam@users.noreply.github.com>
2025-08-20 12:31:38 +0530

    Update spark_transformation_job.py
    spark_transformation file

--- spark_job/spark_transformation_job.py
+++ spark_job/spark_transformation_job.py
@@ -22,7 +22,7 @@ def main(env,bq_project, bq_dataset, transformed_table, route_insights_table, or
         logger.info("spark session initialized")
 
         # Resolve GCS path based on the environment
-        input_path = f"gs://project-airflow-gds/airflow-project-tutorial-1/source-{env}"
+        input_path = f"gs://your file path"
         logger.info(f"Input path resolved: {input_path}")
 
         # Read the data from GCS
